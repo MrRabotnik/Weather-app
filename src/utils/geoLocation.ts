ba556bbd74d0c999e0751531f1d8678f69c2542c
Sargis <terbalyantss@gmail.com>
2024-08-16 22:14:52 +0400

    Update geoLocation.ts

--- src/utils/geoLocation.ts
+++ src/utils/geoLocation.ts
@@ -4,22 +4,29 @@ const getCurrentLocation = (): Promise<{
 }> => {
     return new Promise((resolve, reject) => {
         if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const latitude = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-                    resolve({ latitude, longitude });
-                },
-                (error) => {
-                    reject(`Error (${error.code}): ${error.message}`);
-                },
-                {
-                    enableHighAccuracy: true,
-                    timeout: 5000,
-                    maximumAge: 0,
+            navigator.permissions.query({ name: "geolocation" }).then((result) => {
+                if (result.state === "granted" || result.state === "prompt") {
+                    navigator.geolocation.getCurrentPosition(
+                        (position) => {
+                            const latitude = position.coords.latitude;
+                            const longitude = position.coords.longitude;
+                            resolve({ latitude, longitude });
+                        },
+                        (error) => {
+                            reject(`Error (${error.code}): ${error.message}`);
+                        },
+                        {
+                            enableHighAccuracy: true,
+                            timeout: 5000,
+                            maximumAge: 0,
+                        }
+                    );
+                } else if (result.state === "denied") {
+                    reject("Geolocation access denied.");
                 }
-            );
+            });
         } else {
+            alert("Geolocation is not supported by this browser.");
             reject("Geolocation is not supported by this browser.");
         }
     });
@@ -33,7 +40,7 @@ getCurrentLocation()
         };
     })
     .catch((error) => {
-        console.error(error);
+        console.error("Error retrieving location:", error);
     });
 
 export default getCurrentLocation;
